5851fc12d0a3def6b399a2a62ff6901ebd42ae82
tracymoige <tracymoige@outlook.com>
2023-05-19 21:48:44 +0300

    Added source code

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <small>
+        <a
+          href="https://github.com/tracymoige/Weather-React"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>{" "}
+        by Tracy Moige
+      </small>
+    </div>
+  );
+}
